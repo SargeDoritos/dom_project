7edac1d48f48fef11f8e04865f28f6193cb78e93
Isaac <iselusanmi@stu.allstarcode.org>
2023-07-31 16:45:14 -0400

    bla

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Add your code to this file
-// Function to add a dotted yellow border to the first image and all images with the class "pokemon_images"
 function addDottedBorderToImages() {
   const startersImage = document.getElementById("starters_image_div").querySelector("img");
   startersImage.style.border = "2px dotted yellow";
@@ -10,32 +9,27 @@ function addDottedBorderToImages() {
   }
 }
 
-// Function to change the background color to blue
 function changeBackgroundColorToBlue() {
   document.body.style.backgroundColor = "darkblue";
 }
 
-// Function to underline the text "Pokemon!" in the h1 heading
 function underlinePokemonHeading() {
   const h1Heading = document.querySelector("h1");
   h1Heading.style.textDecoration = "underline";
 }
 
-// Function to update the h2 heading text and center it in the middle
 function updateH2Text() {
   const h2Heading = document.querySelector("h2");
   h2Heading.innerText = "Here are Pokemon's incredible starters!";
   h2Heading.style.textAlign = "center";
-  h2Heading.style.marginTop = "50px"; // Optional: Add some top margin for better positioning
+  h2Heading.style.marginTop = "50px"; 
 }
 
-// Function to update the footer text
 function updateFooterText() {
   const footer = document.querySelector("footer");
   footer.innerText = "ALL STAR CODE - DOM PROJECT";
 }
 
-// Call the functions when the page has finished loading
 window.onload = function () {
   addDottedBorderToImages();
   changeBackgroundColorToBlue();
